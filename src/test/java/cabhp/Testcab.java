20b17c0200f7a0e9a8da242fafc9f12fb69331fc
aquajack22 <aquajack22@gmail.com>
2015-07-27 18:32:36 +0530

    Test changes

--- src/test/java/cabhp/Testcab.java
+++ src/test/java/cabhp/Testcab.java
@@ -8,7 +8,7 @@ import cucumber.api.CucumberOptions;
 @RunWith(Cucumber.class)
 @CucumberOptions(features={"src/test/resources/test.feature"},format={"pretty", "html:target/CucumberHTMLReport","json:target/Cucumber.json"})
 public class Testcab {
-
+System.out.println("");
 }
 
 
